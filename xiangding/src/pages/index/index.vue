2adb8feeae76b6ae297a32f51d3edf7b60a4b54e
lzccheng <1187350675@qq.com>
2018-04-27 11:42:18 +0800

    positionReset

--- xiangding/src/pages/index/index.vue
+++ xiangding/src/pages/index/index.vue
@@ -21,14 +21,14 @@
           <p>
             <span>入住日期：</span>
             <span>
-              <input id="date1" type="text" readonly="" @focus="handleBlur" placeholder="日期选择特效" data-lcalendar="2016-05-11,2016-05-11" v-model="date1_value"/>
+              <input id="date1" type="text" onfocus="this.blur()" readonly="" @focus="handleBlur" placeholder="日期选择特效" data-lcalendar="2016-05-11,2016-05-11" v-model="date1_value"/>
             </span>
             <span><i class="fas fa-angle-right"></i></span>
           </p>
           <p>
             <span>退房日期：</span>
             <span>
-              <input id="date2" type="text" readonly="" placeholder="日期选择特效" data-lcalendar="2016-05-11,2016-05-11" v-model="date2_value"/>
+              <input id="date2" type="text" onfocus="this.blur()" readonly="" placeholder="日期选择特效" data-lcalendar="2016-05-11,2016-05-11" v-model="date2_value"/>
             </span>
             <span><i class="fas fa-angle-right"></i></span>
           </p>
@@ -83,7 +83,7 @@
   				<li>
   					<div class="map">
   						<span class="_right"><i class="fas fa-map-marker-alt"></i></span>
-              <span ref="show_erea"><input id="area" type="text" readonly="" placeholder="城市选择特效"  value="广东省,深圳市,南山区" v-model="area_value"/></span>
+              <span ref="show_erea"><input onfocus="this.blur()" id="area" type="text" readonly="" placeholder="城市选择特效"  value="广东省,深圳市,南山区" v-model="area_value"/></span>
 	  					<span v-if="!show_erea" @click="handleChange_erea">{{text_erea}}</span>
               <input id="value1" type="hidden" value="20,234,504"/> 
   					</div>
@@ -174,6 +174,7 @@
 <script>  
   import Swiper from 'swiper' 
   import star from '../../components/star/star'
+  import common from '../../common/js/common'
 
   let g_date = new Date()
   let _date = new Date()
@@ -194,79 +195,76 @@
       mounted:function(){
 
         let that = this
-<<<<<<< HEAD
         //高德地图
          // 实例化地图， 命名定位插件实例、标记实例、地理编码实例、输出变量
-           const loading = this.$loading({
-            lock: true,
-            text: ' 定位中............',
-            background: 'rgba(0, 0, 0, 0.7)',
-            // target: '.msg'
-          })
-          var map = new AMap.Map('aMap', {
-              resizeEnable: true,
-              zoom: 16
-            }),
-            geolocation = null,
-            geocoder = null
+        //    const loading = this.$loading({
+        //     lock: true,
+        //     text: ' 定位中............',
+        //     background: 'rgba(0, 0, 0, 0.7)',
+        //     // target: '.msg'
+        //   })
+        //   var map = new AMap.Map('aMap', {
+        //       resizeEnable: true,
+        //       zoom: 16
+        //     }),
+        //     geolocation = null,
+        //     geocoder = null
 
-          // 加载地理位置编码插件
-          AMap.service('AMap.Geocoder', function() { //回调函数
-            //实例化Geocoder
-            geocoder = new AMap.Geocoder({
-              city: "010" //城市，默认：“全国”
-            });
-            //TODO: 使用geocoder 对象完成相关功能
-          });
-          // 加载定位插件
-          map.plugin('AMap.Geolocation', function() {
-            // 初始化定位插件
-            geolocation = new AMap.Geolocation({
-              enableHighAccuracy: true, //是否使用高精度定位，默认:true
-              timeout: 10000, //超过10秒后停止定位，默认：无穷大
-              maximumAge: 0, //定位结果缓存0毫秒，默认：0
-              convert: true, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
-              showButton: true, //显示定位按钮，默认：true
-              buttonPosition: 'LB', //定位按钮停靠位置，默认：'LB'，左下角
-              buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-              showMarker: false, //定位成功后在定位到的位置显示点标记，默认：true
-              showCircle: false, //定位成功后用圆圈表示定位精度范围，默认：true
-              panToLocation: true, //定位成功后将定位到的位置作为地图中心点，默认：true
-              zoomToAccuracy: true //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-            });
-            // 把定位插件加入地图实例
-            map.addControl(geolocation);
+        //   // 加载地理位置编码插件
+        //   AMap.service('AMap.Geocoder', function() { //回调函数
+        //     //实例化Geocoder
+        //     geocoder = new AMap.Geocoder({
+        //       city: "010" //城市，默认：“全国”
+        //     });
+        //     //TODO: 使用geocoder 对象完成相关功能
+        //   });
+        //   // 加载定位插件
+        //   map.plugin('AMap.Geolocation', function() {
+        //     // 初始化定位插件
+        //     geolocation = new AMap.Geolocation({
+        //       enableHighAccuracy: true, //是否使用高精度定位，默认:true
+        //       timeout: 10000, //超过10秒后停止定位，默认：无穷大
+        //       maximumAge: 0, //定位结果缓存0毫秒，默认：0
+        //       convert: true, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+        //       showButton: true, //显示定位按钮，默认：true
+        //       buttonPosition: 'LB', //定位按钮停靠位置，默认：'LB'，左下角
+        //       buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+        //       showMarker: false, //定位成功后在定位到的位置显示点标记，默认：true
+        //       showCircle: false, //定位成功后用圆圈表示定位精度范围，默认：true
+        //       panToLocation: true, //定位成功后将定位到的位置作为地图中心点，默认：true
+        //       zoomToAccuracy: true //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+        //     });
+        //     // 把定位插件加入地图实例
+        //     map.addControl(geolocation);
 
-            // 添加地图全局定位事件
-            AMap.event.addListener(geolocation, 'complete', onComplete); //返回定位信息
-            AMap.event.addListener(geolocation, 'error', onError); //返回定位出错信息
+        //     // 添加地图全局定位事件
+        //     AMap.event.addListener(geolocation, 'complete', onComplete); //返回定位信息
+        //     AMap.event.addListener(geolocation, 'error', onError); //返回定位出错信息
 
-            // 调用定位
-            geolocation.getCurrentPosition();
-          });
+        //     // 调用定位
+        //     geolocation.getCurrentPosition();
+        //   });
 
-          /*
-           *解析定位结果
-           */
-          function onComplete(data) {
-            console.log(data)
-            alert(data.formattedAddress+'附近')
-            loading.close()
-          };
-          /*
-           *解析定位错误信息
-           */
-          function onError(data) {
-            console.log(data)
-            alert('Error')
-            loading.close()
+        //   /*
+        //    *解析定位结果
+        //    */
+        //   function onComplete(data) {
+        //     console.log(data)
+        //     alert(data.formattedAddress+'附近')
+        //     loading.close()
+        //   };
+        //   /*
+        //    *解析定位错误信息
+        //    */
+        //   function onError(data) {
+        //     console.log(data)
+        //     alert('Error')
+        //     loading.close()
 
-          };
-        console.log('aMap finsh')
+        //   };
+        // console.log('aMap finsh')
 
         let nowDate = new Date()
-=======
->>>>>>> aaabccdc4e6293fd53f1aba913a8a5c5884200d4
         // let imterval = setInterval(()=>{
         //   if(that.$refs._iframe.contentWindow.local){
         //     // console.log(new Date().getTime(),that.$refs._iframe.contentWindow)
@@ -281,13 +279,10 @@
         //     })
         //     clearInterval(imterval)
         //   }
-<<<<<<< HEAD
         //   if(new Date().getTime()-nowDate.getTime()>5000){
         //     alert('位置请求超时！')
         //     clearInterval(imterval)
         //   }
-=======
->>>>>>> aaabccdc4e6293fd53f1aba913a8a5c5884200d4
         // },50)
         this.$axios({url:'/api/bannerData',data:{id:123}}).then((res)=>{
           that.arrItem = res.data
@@ -531,62 +526,72 @@
          // 实例化地图， 命名定位插件实例、标记实例、地理编码实例、输出变量
            const loading = this.$loading({
             lock: true,
-            text: ' 定位中............',
+            text: ' 定位中..........',
             background: 'rgba(0, 0, 0, 0.7)',
             // target: '.msg'
           })
-          var map = new AMap.Map('aMap', {
-              resizeEnable: true,
-              zoom: 16
-            }),
-            geolocation = null,
-            geocoder = null
+          // var map = new AMap.Map('aMap', {
+          //     resizeEnable: true,
+          //     zoom: 16
+          //   }),
+          //   geolocation = null,
+          //   geocoder = null
 
-          // 加载地理位置编码插件
-          AMap.service('AMap.Geocoder', function() { //回调函数
-            //实例化Geocoder
-            geocoder = new AMap.Geocoder({
-              city: "010" //城市，默认：“全国”
-            });
-            //TODO: 使用geocoder 对象完成相关功能
-          });
-          // 加载定位插件
-          map.plugin('AMap.Geolocation', function() {
-            // 初始化定位插件
-            geolocation = new AMap.Geolocation({
-              enableHighAccuracy: true, //是否使用高精度定位，默认:true
-              timeout: 10000, //超过10秒后停止定位，默认：无穷大
-              maximumAge: 0, //定位结果缓存0毫秒，默认：0
-              convert: true, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
-              showButton: true, //显示定位按钮，默认：true
-              buttonPosition: 'LB', //定位按钮停靠位置，默认：'LB'，左下角
-              buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
-              showMarker: false, //定位成功后在定位到的位置显示点标记，默认：true
-              showCircle: false, //定位成功后用圆圈表示定位精度范围，默认：true
-              panToLocation: true, //定位成功后将定位到的位置作为地图中心点，默认：true
-              zoomToAccuracy: true //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
-            });
-            // 把定位插件加入地图实例
-            map.addControl(geolocation);
+          // // 加载地理位置编码插件
+          // AMap.service('AMap.Geocoder', function() { //回调函数
+          //   //实例化Geocoder
+          //   geocoder = new AMap.Geocoder({
+          //     city: "010" //城市，默认：“全国”
+          //   });
+          //   //TODO: 使用geocoder 对象完成相关功能
+          // });
+          // // 加载定位插件
+          // map.plugin('AMap.Geolocation', function() {
+          //   // 初始化定位插件
+          //   geolocation = new AMap.Geolocation({
+          //     enableHighAccuracy: true, //是否使用高精度定位，默认:true
+          //     timeout: 10000, //超过10秒后停止定位，默认：无穷大
+          //     maximumAge: 0, //定位结果缓存0毫秒，默认：0
+          //     convert: true, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+          //     showButton: true, //显示定位按钮，默认：true
+          //     buttonPosition: 'LB', //定位按钮停靠位置，默认：'LB'，左下角
+          //     buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+          //     showMarker: false, //定位成功后在定位到的位置显示点标记，默认：true
+          //     showCircle: false, //定位成功后用圆圈表示定位精度范围，默认：true
+          //     panToLocation: true, //定位成功后将定位到的位置作为地图中心点，默认：true
+          //     zoomToAccuracy: true //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+          //   });
+          //   // 把定位插件加入地图实例
+          //   map.addControl(geolocation);
 
-            // 添加地图全局定位事件
-            AMap.event.addListener(geolocation, 'complete', onComplete); //返回定位信息
-            AMap.event.addListener(geolocation, 'error', onError); //返回定位出错信息
+          //   // 添加地图全局定位事件
+          //   AMap.event.addListener(geolocation, 'complete', onComplete); //返回定位信息
+          //   AMap.event.addListener(geolocation, 'error', onError); //返回定位出错信息
 
-            // 调用定位
-            geolocation.getCurrentPosition();
-          });
+          //   // 调用定位
+          //   geolocation.getCurrentPosition();
+          // });
 
           /*
            *解析定位结果
            */
+          common.getLocation(onComplete,onError)
           function onComplete(data) {
             console.log(data)
             // that.text_erea = res.surroundingPois[0].title+'附近'
-            that.text_erea = data.formattedAddress+'附近'
+            // alert(data.formattedAddress+'附近')
+            let addr = data.formattedAddress.split('街道')
+            if(!addr[1]){
+              addr = data.formattedAddress.split('号')
+              alert(addr[1])
+            }
+            alert(addr[1])
+            alert_
+            that.text_erea = addr[1]+'附近'
             that.show_erea = false
             that.$refs.show_erea.style.display = 'none'
-            alert(data.formattedAddress+'附近')
+            
+
             loading.close()
           };
           /*
@@ -678,6 +683,7 @@
           // return this.zero(this.value1.getDate())
           let date = new Date(this.date1_value).getTime()
           let date2 = date+1000*60*60*24
+          // alert(new Date('2018/4/20').getDate())
           this.date2_value = new Date(date2).getFullYear()+'-'+(new Date(date2).getMonth()+1)+'-'+new Date(date2).getDate()
           return this.zero(new Date(date).getDate())
         },
